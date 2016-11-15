f72a4efe1ea21ec24d60f6429734cfbca9ce41a4
Roshan Gautam <Roshan Gautam>
2016-11-14 23:51:00 -0600

    added showBoard method

--- Chess/src/Chess.java
+++ Chess/src/Chess.java
@@ -1,5 +1,39 @@
-public class Chess {
-	
-
-}
-
+public class Chess{
+	public static void main(String[]args){
+		String[][] chessBoard = new String[8][8];
+		for(int i = 0; i<chessBoard.length; i++){
+			for(int j = 0; j<chessBoard.length; j++){
+				chessBoard[i][j]=" ";
+			}
+		}
+		String King1,king2 ="K";
+		String Queen1,Queen2 ="Q";
+		String[] Rook1 = new String[2];
+		String[] Rook2 = new String[2];
+		String[] Knight1 = new String[2];
+		String[] Knight2 = new String[2];
+		String[] Bishop1 = new String[2];
+		String[] Bishop2 = new String[2];
+		String[] Pawn1 = new String[8];
+		String[] Pawn2 = new String[8];
+		
+		showBoard(chessBoard);
+		
+		
+	}
+	public static void showBoard(String ChessBoard[][]){
+		System.out.print("  ");
+		for (int k=0; k<ChessBoard.length;k++ ){
+			System.out.print(" "+ k+"");
+		}
+		System.out.println();
+		for(int i = 0; i<ChessBoard.length; i++){		
+			System.out.print(i+" ");
+			for(int j = 0; j<ChessBoard.length; j++){
+				System.out.print("|"+ChessBoard[i][j]);
+				
+			}
+			System.out.println("|");
+		}
+	}
+	}
