2ade68dffacaddcc459238a97a97b2fbc354b8b4
Roshan Gautam <Roshan Gautam>
2016-11-17 15:23:51 -0600

    finalized legalRook for both player1 and player2

--- Chess/src/Chess.java
+++ Chess/src/Chess.java
@@ -1,6 +1,13 @@
 //Game is still long way to go.
+//Things to do.
+//Add moves of pieces
+//Add Turn basis
+//checkmate
+
 import java.util.Scanner;
 public class Chess{
+	
+	
 	public static void main(String[]args){
 		String[][] ChessBoard = new String[8][8];
 		for(int i = 0; i<ChessBoard.length; i++){
@@ -37,7 +44,7 @@ public class Chess{
 			int drow1 = Integer.parseInt(input.next());
 			int dcolumn1 = Integer.parseInt(input.next());
 			// check the piece
-			// call the method to check eligibilty of the move
+			// call the method to check eligibility of the move
 			while (!checkMove(ChessBoard,srow1,scolumn1,drow1,dcolumn1)){
 				System.out.println("Illegal move! please try again");
 				drow1 = Integer.parseInt(input.next());
@@ -53,6 +60,10 @@ public class Chess{
 		}
 		}
 	}
+	
+	
+	
+	
 	public static boolean isPlayer1(String piece){
 		String[] Player1={"P","R","B","K","Q","N"};
 		boolean check=false;
@@ -63,6 +74,9 @@ public class Chess{
 		}
 		return check;
 	}
+	
+	
+	
 	public static boolean isPlayer2(String piece){
 		String[] Player2={"p","r","b","k","q","n"};
 		boolean check=false;
@@ -73,6 +87,9 @@ public class Chess{
 		}
 		return check;
 	}
+	
+	
+	
 	public static boolean checkMove(String board[][],int srow,int scolumn, int drow, int dcolumn){
 		//This is a method for checking the validity of a move. 
 		//All legal* methods are checked in this method to continue on game or to ask user to re-enter drow and dcolumn
@@ -85,8 +102,10 @@ public class Chess{
 		return Check;
 		
 	}
-	public static boolean legalPawn(String board[][],int srow,int scolumn, int drow, int dcolumn){
-		
+	
+	
+	
+	public static boolean legalPawn(String board[][],int srow,int scolumn, int drow, int dcolumn){		
 		boolean checker = false;
 		if((board[srow][scolumn].equals("P"))){
 			if((board[srow+1][scolumn].equals(" "))&&(drow==srow+1)&&( dcolumn==scolumn)){
@@ -108,68 +127,114 @@ public class Chess{
 			}
 			else if(((srow-1)==drow) && ((scolumn-1)==dcolumn)){
 				if(isPlayer1(board[srow-1][scolumn-1]))checker=true;
-			}
-		
+			}		
 		}
 		return checker;
 	}
+	
+	
+	
 	public static boolean legalRook(String board[][],int srow,int scolumn,int drow,int dcolumn){
 		boolean checker = false;
-		if((board[srow][scolumn].equals("R"))){
+		if((board[srow][scolumn].equals("R")||(board[srow][scolumn].equals("r")))){
 			int count=0;
-			//Move R in -Y direction
+			//Move R or r in -Y direction
 			if((scolumn==dcolumn)&&((drow-srow)>0)){
 				for(int i=srow; i<=drow; i++){
 					if (board[i][dcolumn].equals(" ")){
 					count++;
 					}
+					if(isPlayer2(board[i][dcolumn])&&isPlayer1(board[srow][scolumn])){
+						count++;
+					}
+					else if(isPlayer1(board[i][dcolumn])&&isPlayer2(board[srow][scolumn])){
+						count++;
+					}
 				}
-				if ((count==drow-srow)&&(isPlayer2(board[drow][dcolumn])||board[drow][dcolumn].equals(" "))){
+				if ((count==drow-srow)&&(isPlayer1(board[srow][scolumn])) && (isPlayer2(board[drow][dcolumn])||board[drow][dcolumn].equals(" "))){
 				checker=true;
 				}
+				else if ((count==drow-srow)&&(isPlayer2(board[srow][scolumn])) && (isPlayer1(board[drow][dcolumn])||board[drow][dcolumn].equals(" "))){
+					checker=true;
+					}
+				
 			}
-			//Move R in +Y direction
+			//Move R or r in +Y direction
 			if((scolumn==dcolumn)&&((drow-srow)<0)){
 				for(int i=srow; i>=drow; i--){
 					if (board[i][dcolumn].equals(" ")){
 					count++;
 					}
+					if(isPlayer2(board[i][dcolumn])&&isPlayer1(board[srow][scolumn])){
+						count++;
+					}
+					else if(isPlayer1(board[i][dcolumn])&&isPlayer2(board[srow][scolumn])){
+						count++;
+					}
 				}
-				if ((count ==srow-drow)&&(isPlayer2(board[drow][dcolumn])||board[drow][dcolumn].equals(" "))){
+				if ((count ==srow-drow)&& (isPlayer1(board[srow][scolumn])) && (isPlayer2(board[drow][dcolumn])||board[drow][dcolumn].equals(" "))){
 				checker=true;
 				}
+				else if ((count ==srow-drow)&&(isPlayer2(board[srow][scolumn])) && (isPlayer1(board[drow][dcolumn])||board[drow][dcolumn].equals(" "))){
+					checker=true;
+					}
 			}
-			//Move R in -X direction
+			//Move R or r in -X direction
 			if((srow==drow)&&((dcolumn-scolumn)<0)){
 				for(int i=scolumn; i>=dcolumn; i--){
 					if (board[drow][i].equals(" ")){
 					count++;
 					}
+					if(isPlayer2(board[drow][i])&&isPlayer1(board[srow][scolumn])){
+						count++;
+					}
+					else if(isPlayer1(board[drow][i])&&isPlayer2(board[srow][scolumn])){
+						count++;
+					}
 				}
-				if ((count==scolumn-dcolumn)&&(isPlayer2(board[drow][dcolumn])||board[drow][dcolumn].equals(" "))){
+				if ((count==scolumn-dcolumn)&&(isPlayer1(board[srow][scolumn]))&&(isPlayer2(board[drow][dcolumn])||board[drow][dcolumn].equals(" "))){
 				checker=true;
 				}
+				else if ((count==scolumn-dcolumn)&&(isPlayer2(board[srow][scolumn]))&&(isPlayer1(board[drow][dcolumn])||board[drow][dcolumn].equals(" "))){
+					checker=true;
+					}
 			}
-			//Move R in +X direction
+			//Move R or r in +X direction
 			if((srow==drow)&&((dcolumn-scolumn)>0)){
 				for(int i=scolumn; i<=dcolumn; i++){
 					if (board[drow][i].equals(" ")){
 					count++;
 					}
+					if(isPlayer2(board[drow][i])&&isPlayer1(board[srow][scolumn])){
+						count++;
+					}
+					else if(isPlayer1(board[drow][i])&&isPlayer2(board[srow][scolumn])){
+						count++;
+					}
 				}
-				if ((count==dcolumn-scolumn)&&(isPlayer2(board[drow][dcolumn])||board[drow][dcolumn].equals(" "))){
+				if ((count==dcolumn-scolumn)&&(isPlayer1(board[srow][scolumn]))&&(isPlayer2(board[drow][dcolumn])||board[drow][dcolumn].equals(" "))){
 				checker=true;
 				}
+				else if ((count==dcolumn-scolumn)&&(isPlayer2(board[srow][scolumn]))&&(isPlayer1(board[drow][dcolumn])||board[drow][dcolumn].equals(" "))){
+					checker=true;
+					}
 			}
 			//All the movements for player2 are to be added
 			
 		}
 		return checker;
 	}
+	
+	
+	
+	
 	public static void movePiece(String chessBoard[][],int srow,int scolumn, int drow, int dcolumn){
 		chessBoard[drow][dcolumn]=chessBoard[srow][scolumn];
 		chessBoard[srow][scolumn]=" ";
 	}
+	
+	
+	
 	public static void starter(String chessBoard[][]){
 		String King1 = "K";
 		String King2 = "k";
@@ -210,6 +275,9 @@ public class Chess{
 			chessBoard[0][4]=Queen1;
 			chessBoard[7][4]=Queen2;
 	}
+	
+	
+	
 	public static void showBoard(String Board[][]){
 		System.out.print("  ");
 		for (int k=0; k<Board.length;k++ ){
@@ -220,19 +288,24 @@ public class Chess{
 			System.out.print(i+" ");
 			for(int j = 0; j<Board.length; j++){
 				System.out.print("|"+Board[i][j]);
-				
 			}
 			System.out.println("|");
 		}
 	}
+	
+	
+	
+	
 	public static Boolean BlankCheck(String box){
 		Boolean checker=false;
 		if (box.equals(" ")){
 			checker=true;
 		}
-		
 		return checker;
 	}
+	
+	
+	
 	public static String instruction(){
 		String set ="Player1 is Capital\nPlayer2 is small"
 				+ "\nK->king, Q->queen, B->bishop, N->knight, R->rook, P->pawn"
@@ -240,4 +313,5 @@ public class Chess{
 				+ "\nThen Type row and column separated by space and enter to move selected piece to desired place";
 		return set;
 	}
-	}
+	
+}
